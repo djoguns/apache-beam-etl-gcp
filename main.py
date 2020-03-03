1611b70c60df65f7987eb29d1636d820b871f20b
Dayo Oguntoyinbo <doguntoyinbo@mango-solutions.com>
2020-03-03 17:11:28 +0000

    Writing the output data, with Timestamp...

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 
+# Generate Timestamp
+import datetime
+date_now = datetime.datetime.now()
+# date = '2019_10_17' # yyyy_mm_dd
+now = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
+
 # Default PipelineOptions()
 p = beam.Pipeline(options=PipelineOptions())
 
@@ -19,12 +25,12 @@ class GetTotal(beam.DoFn):
 		""" """
 
 		# get the total transactions for one item
-		return print([(str(element[0]), sum(element[1]))])
+		return [(str(element[0]), sum(element[1]))]
 
 # Use a ParDo
 # Uses the beam.ParDo
 data_from_source = (p
-	
+
 	| 'ReadMyFile' >> ReadFromText('input/BreadBasket_DMS.csv')
 	
 	# Using beam.Map to accept lambda function
@@ -43,7 +49,8 @@ data_from_source = (p
 	# | 'Sum using beam.Map' >> beam.Map(lambda record: (record[0],sum(record[1])))
 
 	# Export results to a new file
-	| 'Export results to a new file' >> WriteToText('output/day-list-pardo', '.txt')
+	# You can a timestamp, just for the fun of it.
+	| 'Export results to a new file' >> WriteToText('output/day-list_' + now, '.txt')
 	)
 
 result = p.run()
